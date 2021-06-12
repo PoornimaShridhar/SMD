d0faabc30c3a73c0635bcfeebe3ca33598d53477
Poornima <sridhar.poornima18@gmail.com>
2021-06-12 16:30:45 +0530

    cleaned and improved home page code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import Home from './components/home/home'
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact from="" render={props => <div> <Navbar {...props}/><Home /></div> }/>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
-import img from './doc.jpg';
-import doc from "./diploma.jpg";
-import msc from './msc.jpeg';
-import sai from './sai.jpeg';
-import msrit from './msrit.png';
-import nutri from './nutri.png';
-import eash from "./eash.webp";
-import person from './person.png';
-// import Container from '@material-ui/core/Container';
+import img from './assets/doc.jpg';
+import diploma from "./assets/diploma.jpg";
+import pd from "./assets/pd.jpg";
+import bachelordegree from "./assets/bachelordegree.jpg";
+import msc from './assets/msc.jpeg';
+import sai from './assets/sai.jpeg';
+import msrit from './assets/msrit.png';
+import nutri from './assets/nutri.png';
+import eash from "./assets/eash.webp";
+import person from './assets/person.png';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Row from 'react-bootstrap/Row';
@@ -24,13 +25,12 @@ import TextInfoContent from '@mui-treasury/components/content/textInfo';
 import { useN03TextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/n03';
 import { useLightTopShadowStyles } from '@mui-treasury/styles/shadow/lightTop';    
 import Modal from '@material-ui/core/Modal';
-// import Carousel from 'react-bootstrap/Carousel';
 import Carousel from 'react-material-ui-carousel';
 import { Paper, Button } from '@material-ui/core';
 
 function Item(props)
 {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
   const [modalStyle] = React.useState(getModalStyle);
   const classes = useStyles();
 
@@ -45,7 +45,6 @@ function Item(props)
         <Paper>
             <Grid container >
                 <Grid item xs={12} sm={3}>
-                    {/* <img width="300" height="300" src={props.item.avatar} alt=""/> */}
                     <Avatar alt="Dr. Bharath Kumar" className={classes.large} src={props.item.avatar} style={{marginLeft:"120px"}}/>
                 </Grid>
                 <Grid item xs={12} sm={9}>
@@ -58,17 +57,15 @@ function Item(props)
                     <Button onClick={handleOpen}>Read More...</Button>
                 </Grid>
             </Grid>
-            <Modal
+                    <Modal
                         open={open}
                         onClose={handleClose}
-                        // aria-labelledby="simple-modal-title"
-                        // aria-describedby="simple-modal-description"
                     >
-                            <div style={modalStyle} className={classes.paper}>
+                        <div style={modalStyle} className={classes.paper}>
                             <Typography variant="h6" gutterBottom align="left" style={{fontFamily: 'Roboto Slab', fontSize:'20px', margin:"30px" }}>
-                            {props.item.rm}
+                                {props.item.rm}
                             </Typography>
-                            </div>
+                        </div>
                     </Modal>
         </Paper>
     )
@@ -78,7 +75,7 @@ function rand() {
     return Math.round(Math.random() * 20) - 10;
   }
   
-  function getModalStyle() {
+    function getModalStyle() {
     const top = 50 + rand();
     const left = 50 + rand();
   
@@ -118,9 +115,11 @@ function Home() {
   const shadowStyles = useLightTopShadowStyles();
   const [open, setOpen] = React.useState(false);
   const [modalStyle] = React.useState(getModalStyle);
+  const [image , setImage] = React.useState("");
 
-  const handleOpen = () => {
+  const handleOpen = (img) => {
     setOpen(true);
+    setImage(img);
   };
 
   const handleClose = () => {
@@ -142,108 +141,116 @@ function Home() {
     },
     {
         name: "Jayanta Ghosh",
-        description: "Let me start by saying that Individuals like Dr. Bharath are boon to society. You don’t find many professionals in sports medicine field and even if we do they may not be easily accessible. I chanced upon him while I was searching for sports medicine professional since my tennis elbow treatment had gone wrong while consulting another famous ortho doctor. I had developed a secondary tear in the elbow area. I was suffering because of this for almost half a year then. I met Dr Bharath and he first listened (he is a very good patient listener) to everything that had transpired and then explained why it happened.",
+        description: "I had developed a secondary tear in the elbow area. I was suffering because of this for almost half a year then. I met Dr Bharath.He also explained the exercises required to heal the tennis elbow issue and as to how those exercises will heal the affected area. It has been 2 months now I have recovered 80%.When I met him I did not even have strength to do a hand shake with firm grip and today because of the treatment by Dr. Bharath I have regained that strength. I and my wife also have started consulting him for fat loss and overall health well being for a month now. There are very positive results and I have lost almost 1.5 kgs of fat mass.",
         avatar: person,
         rm:"He also explained the exercises required to heal the tennis elbow issue and as to how those exercises will heal the affected area. It has been 2 months now I have recovered 80%. When I met him I did not even have strength to do a hand shake with firm grip and today because of the treatment by Dr. Bharath I have regained that strength. I and my wife also have started consulting him for fat loss and overall health well being for a month now. There are very positive results and I have lost almost 1.5 kgs of fat mass. The programmatic approach of step by step capturing the data and auditing it is the most important aspect here which guides an individual if something goes wrong. I had been into fat loss journey for 3 years now however my journey for last 1 year had become stagnant. I did not know why! Dr. Bharath conducted few blood parameter tests and we identified the reason. It is a lifetime journey of adhering to healthy lifestyle and we learn everyday from him and his team. Dr. Bharath patiently responds to all our questions <Sorry Sir, I bug you with too many questions :-) > and explains the concept behind it. I am really thankful that we found him."
     }
 ]
 
+    var credentials = [
+        {
+            heading: 'POST GRADUATE DIPLOMA IN SPORTS MEDICINE',
+            body: "Netaji Subhas National Institute of Sports, Patiala ",
+            logo: sai,
+            certificate: diploma
+        },
+        {
+            heading: 'BACHELOR OF MEDICINE & BACHELOR OF SURGERY',
+            body: "M S Ramaiah Medical College, Bangalore",
+            logo: msrit,
+            certificate: bachelordegree
+        },
+        {
+            heading: 'PROFESSIONAL TRAINING IN LOW CARBOHYDRATE HIGH FAT / KETOGENIC TREATMENT',
+            body: "Nutrition Network",
+            logo: nutri,
+            certificate: pd
+        },
+        {
+            heading: 'MSC (DIETETICS AND FOOD SERVICE MANAGEMENT)',
+            body: "Indira Gandhi National Open University (Currently Pursuing)",
+            logo: msc,
+            certificate: "",
+        }
+    ]
+
   return (
      <div>
-    <Container>
-        
-        <Col style={{}}>
-            <Row>
-            {/* <Avatar alt="Dr. Bharath Kumar" className={classes.large} src={img} style={{top:"150px", position:"absolute"}}/> */}
+    <Grid container>
+        <Grid item xs={12} sm={12}>
             <img src={img}  width="300" height="300"  style={{verticalAlign:"top", position:"center", marginTop:"100px"}}/>
-            </Row>
-            <Row>
+            </Grid>
+            <Grid item xs={12} sm={12}>
                 <Typography variant="h3"  gutterBottom style={{fontFamily: 'Roboto Slab'}}>
                     DR. BHARATH KUMAR B
-                    
                 </Typography>
                 <Typography variant="h5"  gutterBottom style={{fontFamily: 'Roboto Slab'}}>
                     <i>MBBS, PGDSM (Sports Medicine)</i>
                 </Typography>
-            </Row>
+            </Grid>
             <br/>
-            <Row>
-            <AiOutlineInstagram size="40"/>
-            <AiOutlineFacebook size="40"/>
-            <AiFillTwitterCircle size="40"/>
-            <AiFillLinkedin size="40"/>
-               
-            </Row>
-        </Col>
+            <Grid item xs={12} sm={12}>
+            <a href="https://www.instagram.com/sportsmedicinedoctor/" target="_blank"><AiOutlineInstagram size="40"/></a>
+            
+            <a href="https://www.facebook.com/SportsMedicineDoctor/" target="_blank"><AiOutlineFacebook size="40"/></a>
 
-       
-
-
-
-    </Container>  
+            <a href="https://twitter.com/sportsdocindia" target="_blank"><AiFillTwitterCircle size="40"/></a>
+            
+            <a href="https://www.linkedin.com/in/sportsmedicinedoctor/" target="_blank"><AiFillLinkedin size="40"/></a>
+            </Grid>
+        </Grid> 
     <br/>
     <br/>
     <Container fluid="true">
     <Grid container spacing={3} style={{backgroundColor:"#d9d8d5"}}>
             <Grid item xs={12} sm={7}>
-            <Typography  variant="h6" align="left" style={{fontFamily: 'Roboto Slab', fontSize:'20px', margin:"30px" , fontWeight:"500"}}>
-                
+                <Typography  variant="h6" align="left" style={{fontFamily: 'Roboto Slab', fontSize:'20px', margin:"30px" , fontWeight:"500"}}>
                     This is Dr. Bharath Kumar B. I am an Allopathic Doctor specialized in Sports Medicine. I graduated from the prestigious 
                     Netaji Subhas National Institute of Sports. 
-                    I am the Founder, Director of Kinesis Sports Clinic and Creator & Program Director of MediFit, an online Medical Weight Loss program.
-              
-                    
-                    </Typography>
+                    I am the Founder, Director of Kinesis Sports Clinic and Creator & Program Director of MediFit, an online Medical Weight Loss program.                
+                </Typography>
             </Grid>
             <Grid item xs={12} sm={1}>
-                                <div style={{ borderWidth: 1, height: 175, backgroundColor: 'black', width: 5, marginLeft: 'auto', marginRight: 'auto' }}></div>
-                                </Grid>                  
+                <div style={{ borderWidth: 1, height: 175, backgroundColor: 'black', width: 5, marginLeft: 'auto', marginRight: 'auto' }}></div>
+            </Grid>                  
 
             <Grid item xs={12} sm={4}>
-            <Typography variant="h6" align="left" style={{fontFamily: 'Roboto Slab', fontSize:'20px', margin:"30px" }}>
+                <Typography variant="h6" align="left" style={{fontFamily: 'Roboto Slab', fontSize:'20px', margin:"30px" }}>
                     My mission is to help the athletes to win Gold medals at the Olympics. He strives to create a Fit India through 
                     Nutrition, Exercise, and Lifestyle. He envisions a world free from lifestyle disorders!
-                    </Typography>
+                </Typography>
             </Grid>
-        </Grid>
-
-        
+        </Grid>    
     </Container>
     
     <br/><br/>
     <Container>
     <Grid container>
             <Grid item xs={12} >
-            <Typography  variant="h3" align="center" style={{fontFamily: 'Roboto Slab', margin:"30px" , fontWeight:"500"}}>
-                
+                <Typography  variant="h3" align="center" style={{fontFamily: 'Roboto Slab', margin:"30px" , fontWeight:"500"}}>
                     My Credentials
-                    
-                    </Typography>
+                </Typography>
             </Grid>
      </Grid>
      <br/>
      <br/>
      <Grid container style={{marginLeft:"50px"}}>
-            <Grid item xs={12} sm={3}>
-                            <Card className={cx(classes.root, shadowStyles.root)}>
-                    <BrandCardHeader
-                        image={sai}
-                        extra={ <button type="button" onClick={handleOpen}>
-                        View Certificate
-                    </button>
-                    }
-                    />
-                    
-                    <CardContent className={classes.content}>
-                        <TextInfoContent
-                        classes={styles}
-                        //   overline={'FACEBOOK INC.'}
-                        heading={'POST GRADUATE DIPLOMA IN SPORTS MEDICINE'}
-                        body={
-                            'Netaji Subhas National Institute of Sports, Patiala'
-                        }
+            {
+                credentials.map( (cred, i) => 
+                
+                <Grid item xs={12} sm={3}>
+                    <Card className={cx(classes.root, shadowStyles.root)}>
+                        <BrandCardHeader
+                            image={cred.logo}
+                            extra={ cred.certificate === "" ? <a href="https://www.kinesissportsclinic.in/" target="_blank">Kinesis Sports Clinic</a> :<button type="button" onClick={()=>{handleOpen(cred.certificate)}}>View Certificate</button> } 
                         />
-                    </CardContent>
+                        <CardContent className={classes.content}>
+                            <TextInfoContent
+                            classes={styles}
+                            heading= {cred.heading}
+                            body= {cred.body}
+                            />
+                        </CardContent>
                     </Card>
                     <Modal
                         open={open}
@@ -251,121 +258,21 @@ function Home() {
                         aria-labelledby="simple-modal-title"
                         aria-describedby="simple-modal-description"
                     >
-                            <div style={modalStyle} className={classes.paper}>
-
-                                <img src={doc} alt="" width="1100" height="700" style={{position:"center"}}/>
-                            </div>
+                        <div style={modalStyle} className={classes.paper}>
+                            <img src={image} alt="Test"  width="1100" height="700" style={{position:"center"}}/>
+                        </div>
                     </Modal>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                            <Card className={cx(classes.root, shadowStyles.root)}>
-                    <BrandCardHeader
-                        image={msrit}
-                        extra={ <button type="button" onClick={handleOpen}>
-                        View Certificate
-                    </button>
-                    }
-                    />
-                    
-                    <CardContent className={classes.content}>
-                        <TextInfoContent
-                        classes={styles}
-                        //   overline={'FACEBOOK INC.'}
-                        heading={'BACHELOR OF MEDICINE & BACHELOR OF SURGERY'}
-                        body={
-                            'M S Ramaiah Medical College, Bangalore'
-                        }
-                        />
-                    </CardContent>
-                    </Card>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="simple-modal-title"
-                        aria-describedby="simple-modal-description"
-                    >
-                            <div style={modalStyle} className={classes.paper}>
-
-                                <img src={doc} alt="" width="1100" height="700" style={{position:"center"}}/>
-                            </div>
-                    </Modal>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                            <Card className={cx(classes.root, shadowStyles.root)}>
-                    <BrandCardHeader
-                        image={nutri}
-                        extra={ <button type="button" onClick={handleOpen}>
-                        View Certificate
-                    </button>
-                    }
-                    />
-                    
-                    <CardContent className={classes.content}>
-                        <TextInfoContent
-                        classes={styles}
-                        //   overline={'FACEBOOK INC.'}
-                        heading={'PROFESSIONAL TRAINING IN LOW CARBOHYDRATE HIGH FAT / KETOGENIC TREATMENT'}
-                        body={
-                            'Nutrition Network'
-                        }
-                        />
-                    </CardContent>
-                    </Card>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="simple-modal-title"
-                        aria-describedby="simple-modal-description"
-                    >
-                            <div style={modalStyle} className={classes.paper}>
-
-                                <img src={doc} alt="" width="1100" height="700" style={{position:"center"}}/>
-                            </div>
-                    </Modal>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                            <Card className={cx(classes.root, shadowStyles.root)}>
-                    <BrandCardHeader
-                        image={msc}
-                        extra={ <button type="button" onClick={handleOpen}>
-                        View Certificate
-                    </button>
-                    }
-                    />
-                    
-                    <CardContent className={classes.content}>
-                        <TextInfoContent
-                        classes={styles}
-                        //   overline={'FACEBOOK INC.'}
-                        heading={'MSC (DIETETICS AND FOOD SERVICE MANAGEMENT)'}
-                        body={
-                            'Indira Gandhi National Open University (Currently Pursuing)'
-                        }
-                        />
-                    </CardContent>
-                    </Card>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="simple-modal-title"
-                        aria-describedby="simple-modal-description"
-                    >
-                            <div style={modalStyle} className={classes.paper}>
-
-                                <img src={doc} alt="" width="1100" height="700" style={{position:"center"}}/>
-                            </div>
-                    </Modal>
-            </Grid>
+                </Grid>
+                )
+            }
      </Grid>
     </Container>
     <br/><br/><br/>
     <Container>
         <Grid container >
                 <Grid item xs={12} >
-                <Typography  variant="h3" align="center" style={{fontFamily: 'Roboto Slab', margin:"30px" , fontWeight:"500"}}>
-                    
-                        Testimonials
-                        
+                <Typography  variant="h3" align="center" style={{fontFamily: 'Roboto Slab', margin:"30px" , fontWeight:"500"}}>                 
+                    Testimonials     
                 </Typography>
                 </Grid>
         </Grid>
@@ -375,9 +282,7 @@ function Home() {
                 items.map( (item, i) => <Item key={i} item={item} /> )
             }
         </Carousel>
-        
     </Container>
-    
     </div>
   );
 }
